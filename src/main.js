923026591a8983dbebaa5bba8cb2767b9f57758b
Calamateo <dany.calamateo@gmail.com>
2022-06-29 12:21:34 -0500

    Se añadieron funcionalidades a los botones y se añadieron el cambio de interfaz

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ async function getTrendingMoviesPreview() {
 	const movies = data.results;
 
 	movies.forEach(movie => {
-		const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+		const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList')
 
 		const movieContainer = document.createElement('div');
 		movieContainer.classList.add('movie-container');
@@ -25,7 +25,7 @@ async function getTrendingMoviesPreview() {
 		movieImg.setAttribute('src', `https://image.tmdb.org/t/p/w300/${movie.poster_path}`)
 
 		movieContainer.appendChild(movieImg);
-		trendingPreviewMoviesContainer.appendChild(movieContainer);
+		trendingMoviesPreviewList.appendChild(movieContainer);
 	})
 }
 
@@ -34,7 +34,7 @@ async function getCategoriesPreview() {
 	const categories = data.genres;
 
 	categories.forEach(categoria => {
-		const categoresPreviewContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+		const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list')
 
 		const categoryContainer = document.createElement('div');
 		categoryContainer.classList.add('category-container');
@@ -46,10 +46,7 @@ async function getCategoriesPreview() {
 
 		categoryTitle.appendChild(categoryText);
 		categoryContainer.appendChild(categoryTitle);
-		categoresPreviewContainer.appendChild(categoryContainer);
+		categoriesPreviewList.appendChild(categoryContainer);
 	})
 
 }
-
-getTrendingMoviesPreview();
-getCategoriesPreview();
